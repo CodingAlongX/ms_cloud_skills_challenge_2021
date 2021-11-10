68cd12a69319ade8f5f0dc72e5684f8e88cedbc6
S. M. JAHANGIR <smjxpro@gmail.com>
2021-11-11 01:31:52 +0600

    Improve the developer experience of an API with Swagger documentation

---
+++ PrintFramerAPI/Program.cs
@@ -0,0 +1,49 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "PrintFarmer API",
+        Description = "Calculates the cost of a picture frame based on its dimensions.",
+        TermsOfService = new Uri("https://go.microsoft.com/fwlink/?LinkID=206977"),
+        Contact = new OpenApiContact
+        {
+            Name = "Your name",
+            Email = string.Empty,
+            Url = new Uri("https://www.microsoft.com/learn")
+
+        }
+    });
+    
+    // Set the comments path for the Swagger JSON and UI
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
